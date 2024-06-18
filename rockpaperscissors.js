2effadd7803d42658f5b878d11dfd9f76f3d5f50
Zachariah Moorhaus <zmoorhaus@gmail.com>
2024-06-18 15:26:49 -0400

    create function to randomly generate items: rock, paper, scissors.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,40 @@
-console.log("helloooo")
+console.log("hello0000 world")
+
+
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// when the user inputs 'rock'
+// generate a random response of 'rock', 'paper' or 'scissors'
+// if response is 'rock', print 'draw'
+// if output is 'paper', print 'lose' 
+// if output is 'scissors', print win
+
+// when the user inputs 'paper'
+// generate a random response of 'rock', 'paper' or 'scissors'
+// if response is 'rock', print 'win'
+// if output is 'paper', print 'draw' 
+// if output is 'scissors', print 'lose'
+
+// when the user inputs 'scissors'
+// generate a random response of 'rock', 'paper' or 'scissors'
+// if response is 'rock', print 'lose'
+// if output is 'paper', print 'win' 
+// if output is 'scissors', print 'draw'
+
+//if 
