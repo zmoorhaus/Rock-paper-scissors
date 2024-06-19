65cd16d27c7e14a65afa1047fc6159c3a09ebcec
Zachariah Moorhaus <zmoorhaus@gmail.com>
2024-06-19 19:57:26 -0400

    add function playRound

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,17 +1,51 @@
 console.log("hello0000 world")
 
+let humanScore = 0;
+let computerScore = 0;
+
 
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoice;
+    const choices = ["rock", "paper", "scissors"];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log("This computer chose fuckin' " + computerChoice)
+    return computerChoice;
 }
 
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+    if ((humanChoice === "rock") || (humanChoice === "paper") || (humanChoice === "scissors")) {
+        console.log ("Your choice was " + humanChoice);
+        return humanChoice;
+    } else {  
+        alert("nah dawg");
+    }
+}
 
+function playRound(humanChoice, ComputerChoice) {
+   if (humanChoice === ComputerChoice) {
+    console.log("HOLY SHIT IT'S A TIE"); 
+    alert("fuck, they also chose that... it's a tie lmao")
+   } else if ((humanChoice === "rock") && (ComputerChoice === "paper") || (humanChoice === "paper") && (ComputerChoice === "scissors") 
+   || (humanChoice === "scissors") && (ComputerChoice === "rock")) {
+    computerScore ++;
+    console.log("Computer Score is " + (computerScore ++));
+    console.log("you lost bitch lmao"); 
+    alert("you lost bitch lmao")  
+   } else if ((humanChoice === "rock") && (ComputerChoice === "scissors") || (humanChoice === "paper") && (ComputerChoice === "rock") || (humanChoice === "scissors") && (ComputerChoice === "paper")) {
+    humanScore ++;
+    console.log("DAMN dude, you are a FUCKING champion")
+    console.log("Your score is " + (humanScore ++))
+    alert("YOU WIN HELL YEAH");
+   }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+playRound(humanSelection, computerSelection);
 
+function playGame() {
 
-
+}
 
 
 
@@ -35,6 +69,4 @@ function getComputerChoice() {
 // generate a random response of 'rock', 'paper' or 'scissors'
 // if response is 'rock', print 'lose'
 // if output is 'paper', print 'win' 
-// if output is 'scissors', print 'draw'
-
-//if 
+// if output is 'scissors', print 'draw; 
